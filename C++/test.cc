1341350b1a9e12889078d54aa9e1f8c3d4f65163
bochao.dang <272708269@qq.com>
2020-06-24 23:59:20 +0800

    1. currently the python can be debugged correctly 2. c++ debugger still has some problem, it cannot hit the breakpoint 3. The .vscode can a template for the vscode configuration

--- C++/test.cc
+++ C++/test.cc
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
 int main() {
+	
 	printf("hello git\n");
+	printf("test for the debugger\n");
 }
--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,4 @@
+import numpy as np
+
 print("this is the first python")
+print("this is a test for breakpoint")
