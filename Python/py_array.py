288753073c0f12cfadc3230eccc712a99d62c271
bochao.dang <272708269@qq.com>
2021-06-03 15:08:17 +0800

    1. add a new file to show the array index

---
+++ Python/py_array.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+array = [2,3,1]
+print(array[1:]) # print[3,1] it means that the print is from the 1 to the end
+print(array[:-1]) # it mean that the print is from the 0 to the element before -1 so the index is a range like [) , 
+                  #the left is close and the righ is open
+                
